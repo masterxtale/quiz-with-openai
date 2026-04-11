1a8c1dc52f4be86b9adaba280ba2d8000ef748a8
immAURORA <idreew2015@gmail.com>
2026-04-11 13:23:42 -0300

    feat: melhorias na UI e integração da aplicação
        - adiciona ícones na tela de temas
        - corrige integração com a API
        - melhora o estilo do questionário
        - adiciona botão de voltar
        - atualiza temas dos quizzes"

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,18 @@ import BodyQuiz from "./components/bodyQuiz.jsx";
 function App() {
 
   const temas = [
-    { nome: "Minecraft", imagem: "public/minecraft.png" },
-    { nome: "História", imagem: "" },
-    { nome: "Geografia", imagem: "" },
-    { nome: "Programação", imagem: "" },
-    { nome: "Filmes", imagem: "" },
-    { nome: "Séries", imagem: "" },
-    { nome: "Animes", imagem: "" },
-    { nome: "Jogos", imagem: "" },
-    { nome: "Esportes", imagem: "" },
-    { nome: "Ciência", imagem: "" },
-    { nome: "Tecnologia", imagem: "" },
-    { nome: "Curiosidades", imagem: "" }
+    { nome: "Minecraft", imagem: "minecraft.png" },
+    { nome: "Pokemon", imagem: "https://i.pinimg.com/originals/a9/42/ff/a942ffab4c8a80be2751d3bfaab9f96d.gif" },
+    { nome: "digimon", imagem: "digimon.png" },
+    { nome: "comida", imagem: "comida.png" },
+    { nome: "Filmes", imagem: "https://i.pinimg.com/originals/ee/77/0f/ee770f48731f6b32ec1a3609c594981a.gif" },
+    { nome: "Séries", imagem: "https://i.pinimg.com/1200x/39/37/47/393747d57d29232eaa98b9ecba7c4dca.jpg" },
+    { nome: "Animes", imagem: "https://i.pinimg.com/736x/43/53/07/4353070dcf5836d3c4b5373f0a592d5e.jpg" },
+    { nome: "Jogos", imagem: "https://i.pinimg.com/originals/02/79/24/02792408d2e99b4a100e81f9c900cb1a.gif" },
+    { nome: "memes  BR", imagem: "https://i.pinimg.com/originals/9d/ad/bc/9dadbc7c182a7c883320bb1506333f68.gif" },
+    { nome: "Programação", imagem: "https://i.pinimg.com/originals/7e/99/02/7e99020ea566ae9b31bed25f6b53a7d2.gif" },
+    { nome: "Geografia", imagem: "https://i.pinimg.com/originals/e6/ed/1b/e6ed1bcf97af0790d6491445c0112221.gif" },
+    { nome: "Tecnologia", imagem: "https://i.pinimg.com/originals/63/7a/4d/637a4d42e1738853b2f96ffbbd60622a.gif" }
   ];
 
   async function receberQuiz(tema) {
